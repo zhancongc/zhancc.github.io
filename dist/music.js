c736a73ef006093ddefd834bbd1d6d1982774429
zhancc <1227753320@qq.com>
2019-10-22 18:14:49 +0800

    Site updated: 2019-10-22 18:14:48

--- dist/music.js
+++ dist/music.js
@@ -8,7 +8,7 @@ const ap = new APlayer({
         artist: '牛奶咖啡',
         url: 'https://music.163.com/song/media/outer/url?id=33756016.mp3',
         cover: 'https://p1.music.126.net/1Al0vrBmm5-HtNXyJH953w==/7952767605403009.jpg',
-	lrc: 'lrc/明天你好.lrc',
+	lrc: '../lrc/明天你好.lrc',
       },  
     ]
 });
